48a35782942b93b8758e0299e0582fdf73cc2044
wuttivas <Savittuw@hotmail.com>
2017-09-10 18:06:47 +0700

    Newwww

---
+++ Assets/_Scripts/BattleSceneController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSceneController : MonoBehaviour
+{
+    public int gridRows = 8;
+    public int gridColumns = 8;
+    public GameObject gridGameObject;
+    public Transform firstCreateGridPosition;
+    public Transform gridParent;
+    public GameObject[] allCharacter;
+    public List<GridPosition> allGrid = new List<GridPosition>();
+
+    public struct GridPosition
+    {
+        public int myRow;
+        public int myColumn;
+        public GameObject myGameObject;
+
+        public bool getGridPosition(int inputRow, int inputColumn)
+        {
+            if (inputRow == myRow && inputColumn == myColumn)
+                return true;
+            return false;
+        }
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Init()
+    {
+        GridGenerator();
+        CharacterInserter(0, 0, 0);
+    }
+
+    public void GridGenerator()
+    {
+        //Grid generator
+        for (int i = 0; i < gridRows; i++)
+        {
+            for (int j = 0; j < gridColumns; j++)
+            {
+                GameObject newGridGameObject = Instantiate(gridGameObject, firstCreateGridPosition.transform.position, Quaternion.identity);
+                newGridGameObject.transform.Translate(Vector3.right * j);
+                newGridGameObject.transform.Translate(Vector3.forward * i);
+                newGridGameObject.transform.SetParent(gridParent);
+                GridPosition newGridPosition = new GridPosition();
+                newGridPosition.myRow = i;
+                newGridPosition.myColumn = j;
+                newGridPosition.myGameObject = newGridGameObject;
+                allGrid.Add(newGridPosition);
+            }
+        }
+    }
+
+    public void CharacterInserter(int inputRow, int inputColumn, int inputCharacter)
+    {
+        //Character inserter
+        for (int i = 0; i < allGrid.Count; i++)
+        {
+            if (allGrid[i].getGridPosition(inputRow, inputColumn))
+            {
+                GameObject newCharacterGameObject = Instantiate(allCharacter[inputCharacter], allGrid[i].myGameObject.transform.position, Quaternion.identity);
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //Combat
+    public string myName;
+    public int myTeam;
+    public int myState;
+    public float myHealthPoint;
+    public float mySpiritPoint;
+    public float myPhysicalDamage;
+    public float myMagicalDamage;
+    public float myPhysicalArmor;
+    public float myMagicalArmor;
+    public float myMovementDistance;
+    public float myAttackRange;
+    //Non-Combat
+    public bool isRevert;
+    public float nonAttackMoveSpeed;
+    public Transform rotateGameObject;
+    //
+    private Animator getAnimator;
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        if (isRevert)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rotateGameObject.rotation = Quaternion.Euler(-90.0f, 45.0f, 0);
+                if (Input.GetKey(KeyCode.A))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 0.0f, 0);
+                if (Input.GetKey(KeyCode.D))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 90.0f, 0);
+            }
+            else if (!Input.GetKey(KeyCode.S))
+            {
+                if (Input.GetKey(KeyCode.A))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, -45.0f, 0);
+                if (Input.GetKey(KeyCode.D))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 135.0f, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rotateGameObject.rotation = Quaternion.Euler(-90.0f, 235.0f, 0);
+                if (Input.GetKey(KeyCode.A))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 270.0f, 0);
+                if (Input.GetKey(KeyCode.D))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 180.0f, 0);
+            }
+            else if (!Input.GetKey(KeyCode.W))
+            {
+                if (Input.GetKey(KeyCode.A))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, -45.0f, 0);
+                if (Input.GetKey(KeyCode.D))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 135.0f, 0);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += -transform.forward * nonAttackMoveSpeed * Time.deltaTime;
+                getAnimator.SetInteger("KEY", 1);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += transform.right * nonAttackMoveSpeed * Time.deltaTime;
+                getAnimator.SetInteger("KEY", 1);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += transform.forward * nonAttackMoveSpeed * Time.deltaTime;
+                getAnimator.SetInteger("KEY", 1);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += -transform.right * nonAttackMoveSpeed * Time.deltaTime;
+                getAnimator.SetInteger("KEY", 1);
+            }
+            if ((Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)))
+                getAnimator.SetInteger("KEY", 0);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rotateGameObject.rotation = Quaternion.Euler(-90.0f, 45.0f, 0);
+                if (Input.GetKey(KeyCode.A))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 0.0f, 0);
+                if (Input.GetKey(KeyCode.D))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 90.0f, 0);
+            }
+            else if (!Input.GetKey(KeyCode.S))
+            {
+                if (Input.GetKey(KeyCode.A))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, -45.0f, 0);
+                if (Input.GetKey(KeyCode.D))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 135.0f, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                rotateGameObject.rotation = Quaternion.Euler(-90.0f, 235.0f, 0);
+                if (Input.GetKey(KeyCode.A))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 270.0f, 0);
+                if (Input.GetKey(KeyCode.D))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 180.0f, 0);
+            }
+            else if (!Input.GetKey(KeyCode.W))
+            {
+                if (Input.GetKey(KeyCode.A))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, -45.0f, 0);
+                if (Input.GetKey(KeyCode.D))
+                    rotateGameObject.rotation = Quaternion.Euler(-90.0f, 135.0f, 0);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += transform.forward * nonAttackMoveSpeed * Time.deltaTime;
+                getAnimator.SetInteger("KEY", 1);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += -transform.right * nonAttackMoveSpeed * Time.deltaTime;
+                getAnimator.SetInteger("KEY", 1);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += -transform.forward * nonAttackMoveSpeed * Time.deltaTime;
+                getAnimator.SetInteger("KEY", 1);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += transform.right * nonAttackMoveSpeed * Time.deltaTime;
+                getAnimator.SetInteger("KEY", 1);
+            }
+            if ((Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D)))
+                getAnimator.SetInteger("KEY", 0);
+        }
+
+    }
+
+    public void Init()
+    {
+        getAnimator = GetComponent<Animator>();
+    }
+}
