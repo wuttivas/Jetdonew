ca034141388b27ba339baf1c5e364b491a4fa6f1
wuttivas <savittuw@hotmail.co.th>
2017-12-09 15:36:19 +0700

    world เป็นอีกแล้ว

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -55,9 +55,16 @@ public class MouseLook : MonoBehaviour {
 	}
 	
 	void Start ()
-	{
-		// Make the rigid body not change rotation
-		if (GetComponent<Rigidbody>())
-			GetComponent<Rigidbody>().freezeRotation = true;
-	}
+    {
+        // Make the rigid body not change rotation
+        NewMethod();
+    }
+
+    private void NewMethod()
+    {
+        if (rigidbody)
+        {
+            rigidbody.freezeRotation = true;
+        }
+    }
 }
