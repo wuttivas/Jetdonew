db1b1f24326a7fbf275ae0ac973167f2a8e63b9c
wuttivas <savittuw@hotmail.co.th>
2017-12-09 19:13:57 +0700

    Revert "world เป็นอีกแล้ว"
    This reverts commit ca034141388b27ba339baf1c5e364b491a4fa6f1.

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -55,16 +55,9 @@ public class MouseLook : MonoBehaviour {
 	}
 	
 	void Start ()
-    {
-        // Make the rigid body not change rotation
-        NewMethod();
-    }
-
-    private void NewMethod()
-    {
-        if (rigidbody)
-        {
-            rigidbody.freezeRotation = true;
-        }
-    }
+	{
+		// Make the rigid body not change rotation
+		if (GetComponent<Rigidbody>())
+			GetComponent<Rigidbody>().freezeRotation = true;
+	}
 }
