34d123468a2cd2c45b424810f80b50a12ac835de
wuttivas <savittuw@hotmail.co.th>
2018-03-23 23:37:52 +0700

    ๊upbase

---
+++ Assets/_Scripts/TacticsMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticsMove : MonoBehaviour
+{
+
+    protected void Init()
+    {
+        
+    }
+
+
+
+}
---
+++ Assets/_Scripts/Tile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool current = false;
+    public bool target = false;
+    public bool selectable = false;
+
+    public List<Tile> adjacencyList = new List<Tile>();
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (current)
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else if (target)
+            NewMethod();
+        else if (target)
+        {
+            GetComponent<Renderer>.material.color = Color.red;
+        }
+        else
+        {
+            GetComponent<Renderer>.material.color = Color.white;
+        }
+    }
+
+    private void NewMethod()
+    {
+        GetComponent<Renderer>.material.color = Color.green;
+    }
+}
